ffb0793700eb269094ccca0ba45e20b57d7b4a4e
Hoang Pham <hphamnk@yahoo.com>
2013-10-22 21:51:31 -0500

    extracted kyb.rar

---
+++ KnowYourBible/src/team/iknowthatscripture/MainActivity.java
@@ -0,0 +1,38 @@
+package team.iknowthatscripture;
+
+
+import team.iknowthatscripture.database.Scriptures;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	// handle choice from options menu
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// create a new Intent to launch
+		Intent listScriptures = 
+				new Intent(this, Scriptures.class);
+		startActivity(listScriptures); 
+		return super.onOptionsItemSelected(item); 
+	} 	
+	
+	
+} // end of public class MainActivity extends Activity {
