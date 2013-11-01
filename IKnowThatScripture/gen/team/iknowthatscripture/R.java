745ab17f1b2851378338c07b75da797cc7724f90
Hoang Pham <hphamnk@yahoo.com>
2013-11-01 05:55:18 -0500

    updated Hoang's iKnowThatScripture

--- IKnowThatScripture/gen/team/iknowthatscripture/R.java
+++ IKnowThatScripture/gen/team/iknowthatscripture/R.java
@@ -29,12 +29,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int icn_dropdown_checked=0x7f020001;
         public static final int icn_dropdown_open=0x7f020002;
+        public static final int nice_button_enabled=0x7f020003;
     }
     public static final class id {
         public static final int addscripture=0x7f09000b;
         public static final int booktext=0x7f090006;
         public static final int button_submit=0x7f090004;
-        public static final int button_text=0x7f090000;
         public static final int chaptertext=0x7f090007;
         public static final int passagetext=0x7f090005;
         public static final int scriptureListView=0x7f090009;
@@ -42,6 +42,7 @@ public final class R {
         public static final int spinner_book=0x7f090001;
         public static final int spinner_chapter=0x7f090002;
         public static final int spinner_verse=0x7f090003;
+        public static final int textview_text=0x7f090000;
         public static final int versetext=0x7f090008;
     }
     public static final class layout {
@@ -107,5 +108,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int NiceButton=0x7f070002;
     }
 }
--- IKnowThatScripture/src/team/iknowthatscripture/MainActivity.java
+++ IKnowThatScripture/src/team/iknowthatscripture/MainActivity.java
@@ -1,18 +1,32 @@
 package team.iknowthatscripture;
 
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		displayVerse();
+		
+		addItemsOnSpinnerBook();
+		addItemsOnSpinnerChapter();
+		addItemsOnSpinnerVerse();
 	}
 
 	@Override
@@ -32,6 +46,48 @@ public class MainActivity extends Activity {
 		startActivity(listScriptures); 
 		return super.onOptionsItemSelected(item); 
 	} 	
+
+	private void displayVerse()
+	{
+		String exampleText = getResources().getString(R.string.joke_honesty) ;
+		TextView textview_text = (TextView)  findViewById(R.id.textview_text);
+		textview_text.setText(exampleText);
+	}
 	
+	private void addItemsOnSpinnerBook()
+	{
+		Spinner spinnerBook = (Spinner) findViewById(R.id.spinner_book);
+		List<String> list = new ArrayList<String>();
+		list.add("book new 0");
+		list.add("book new 1");
+		list.add("book new 2");
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,list);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinnerBook.setAdapter(dataAdapter);
+	}
+	
+	private void addItemsOnSpinnerChapter()
+	{
+		Spinner spinnerChapter = (Spinner) findViewById(R.id.spinner_chapter);
+		List<String> list = new ArrayList<String>();
+		list.add("chapter new 0");
+		list.add("chapter new 1");
+		list.add("chapter new 2");
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,list);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinnerChapter.setAdapter(dataAdapter);
+	}
+	
+	private void addItemsOnSpinnerVerse()
+	{
+		Spinner spinnerVerse = (Spinner) findViewById(R.id.spinner_verse);
+		List<String> list = new ArrayList<String>();
+		list.add("verse new 0");
+		list.add("verse new 1");
+		list.add("verse new 2");
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,list);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinnerVerse.setAdapter(dataAdapter);
+	}
 	
 } // end of public class MainActivity extends Activity {
