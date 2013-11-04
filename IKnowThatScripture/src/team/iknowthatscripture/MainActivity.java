fa3a1811aa2d1933a38fee7e28f8d0d7efbabdfc
Hoang Pham <hphamnk@yahoo.com>
2013-11-04 05:38:12 -0600

    update iknowthatscrpture

--- IKnowThatScripture/src/team/iknowthatscripture/MainActivity.java
+++ IKnowThatScripture/src/team/iknowthatscripture/MainActivity.java
@@ -9,12 +9,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+//	private Spinner spinnerBook = (Spinner) findViewById(R.id.spinner_book);
+//	private Spinner spinnerChapter = (Spinner) findViewById(R.id.spinner_chapter);
+//	private Spinner spinnerVerse = (Spinner) findViewById(R.id.spinner_verse);
 	
 	
 	@Override
@@ -27,6 +34,7 @@ public class MainActivity extends Activity {
 		addItemsOnSpinnerBook();
 		addItemsOnSpinnerChapter();
 		addItemsOnSpinnerVerse();
+		addListenerOnButton();
 	}
 
 	@Override
@@ -88,6 +96,64 @@ public class MainActivity extends Activity {
 		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,list);
 		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinnerVerse.setAdapter(dataAdapter);
+	}
+	
+	private void addListenerOnButton()
+	{
+		final Spinner spinnerBook = (Spinner) findViewById(R.id.spinner_book);
+		final Spinner spinnerChapter = (Spinner) findViewById(R.id.spinner_chapter);
+		final Spinner spinnerVerse = (Spinner) findViewById(R.id.spinner_verse);
+		
+		Button buttonSubmit = (Button) findViewById(R.id.button_submit);
+		
+		final String correctBook = "book new 0";
+		final String correctChapter = "chapter new 0";
+		final String correctVerse = "verse new 0";
+		
+		
+		
+		buttonSubmit.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+//				Toast.makeText(MainActivity.this, 
+//						"OnClickListener : " +
+////								"\nSpinnerBook : "+ String.valueOf(spinnerBook.getSelectedItem()) + 
+////								"\nSpinnerChapter : "+ String.valueOf(spinnerChapter.getSelectedItem()) + 
+////								"\nSpinnerVerse : "+ String.valueOf(spinnerVerse.getSelectedItem()) 
+//								"\nSpinnerBook : "+ String.valueOf(spinnerBook.getSelectedItem()) + 
+//								"\nSpinnerChapter : "+ String.valueOf(spinnerChapter.getSelectedItem()) + 
+//								"\nSpinnerVerse : "+ String.valueOf(spinnerVerse.getSelectedItem()) 
+//								, Toast.LENGTH_SHORT).show();
+				
+				String userBook = String.valueOf(spinnerBook.getSelectedItem());
+				String userChapter = String.valueOf(spinnerChapter.getSelectedItem());
+				String userVerse = String.valueOf(spinnerVerse.getSelectedItem());
+				
+				if ( correctBook.equals(userBook) == true )
+				{
+					Toast.makeText(MainActivity.this, 
+							"Correct: " +
+									"\nSpinnerBook : "+ String.valueOf(spinnerBook.getSelectedItem()) +  
+									"\nSpinnerChapter : "+ String.valueOf(spinnerChapter.getSelectedItem()) + 
+									"\nSpinnerVerse : "+ String.valueOf(spinnerVerse.getSelectedItem()) 
+									, Toast.LENGTH_SHORT).show();
+				}
+				else
+				{
+					Toast.makeText(MainActivity.this, 
+							"Incorrect: " +
+									"\nSpinnerBook : "+ String.valueOf(spinnerBook.getSelectedItem()) + 
+									"\nSpinnerChapter : "+ String.valueOf(spinnerChapter.getSelectedItem()) + 
+									"\nSpinnerVerse : "+ String.valueOf(spinnerVerse.getSelectedItem()) 
+									, Toast.LENGTH_SHORT).show();
+				}
+
+				
+	
+			}
+		});
+		
+		
 	}
 	
 } // end of public class MainActivity extends Activity {
