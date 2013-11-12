5e2a605649acb92a7fee76b7f5364cea3839e660
chhernandez <carloshernandez@utexas.edu>
2013-11-12 13:20:50 -0600

    added help and about dialog boxes
    added help and about dialog boxes

--- KnowYourBible/src/team/kyb/MainActivity.java
+++ KnowYourBible/src/team/kyb/MainActivity.java
@@ -9,15 +9,24 @@ import team.kyb.iknow.IKnowGame;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CursorAdapter;
 
 public class MainActivity extends Activity {
+	
+	static final int DIALOG_HELP_ID = 0;
+	static final int DIALOG_ABOUT_ID = 1;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -94,139 +103,62 @@ public class MainActivity extends Activity {
 		case R.id.settings:
 			return true;
 		case R.id.help:
+			showDialog(DIALOG_HELP_ID);
 			return true;
 /*		case R.id.exit:
 			
 			return true;*/
 		case R.id.about:
+			showDialog(DIALOG_ABOUT_ID);
 			return true;
 		default:		
 			return super.onOptionsItemSelected(item); 
-		}			
 			
-	} 	
-	
-/*	@Override
-	protected void onResume() {
-		super.onResume(); 
-
-
-	} */		
-	
-	
-	
-/*	AsyncTask<Object, Object, Object> preScriptureTask = 
-			new AsyncTask<Object, Object, Object>() {
-
-		@Override
-		protected Object doInBackground(Object... params) { 
-
-			preInsertScripture(); 
-			return null;
-		} 
-
-		@Override
-		protected void onPostExecute(Object result) { 
-			//finish(); 
-			//databaseConnector.close();
-		} 
-	}; 
-
-	private void preInsertScripture(){
+		}
 		
-		//get DatabaseConnector to interact with the SQLite database
-		DatabaseConnector databaseConnector = new DatabaseConnector(this);
-		
-		boolean dbExist = databaseConnector.checkDataBase();	
-		
-		if (dbExist){
-	    	  // do nothing database already exists
-				Log.d("dbExist", "True");	
-	    } else {
-	    	  Log.d("dbExist", "False");
-	    	 
-  				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a1),
-						getResources().getString(R.string.passage_b1), 
-						Integer.parseInt(getResources().getString(R.string.passage_c1)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d1))
-				);
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a2),
-						getResources().getString(R.string.passage_b2), 
-						Integer.parseInt(getResources().getString(R.string.passage_c2)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d2))
-				);
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a3),
-						getResources().getString(R.string.passage_b3), 
-						Integer.parseInt(getResources().getString(R.string.passage_c3)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d3))
-				);
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a4),
-						getResources().getString(R.string.passage_b4), 
-						Integer.parseInt(getResources().getString(R.string.passage_c4)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d4))
-				);								
-  				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a5),
-						getResources().getString(R.string.passage_b5), 
-						Integer.parseInt(getResources().getString(R.string.passage_c5)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d5))
-				);
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a6),
-						getResources().getString(R.string.passage_b6), 
-						Integer.parseInt(getResources().getString(R.string.passage_c6)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d6))
-				);
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a7),
-						getResources().getString(R.string.passage_b7), 
-						Integer.parseInt(getResources().getString(R.string.passage_c7)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d7))
-				);
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a8),
-						getResources().getString(R.string.passage_b8), 
-						Integer.parseInt(getResources().getString(R.string.passage_c8)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d8))
-				);					
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a9),
-						getResources().getString(R.string.passage_b9), 
-						Integer.parseInt(getResources().getString(R.string.passage_c9)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d9))
-				);								
-  				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a10),
-						getResources().getString(R.string.passage_b10), 
-						Integer.parseInt(getResources().getString(R.string.passage_c10)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d10))
-				);
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a11),
-						getResources().getString(R.string.passage_b11), 
-						Integer.parseInt(getResources().getString(R.string.passage_c11)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d11))
-				);
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a12),
-						getResources().getString(R.string.passage_b12), 
-						Integer.parseInt(getResources().getString(R.string.passage_c12)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d12))
-				);
-				databaseConnector.insertScripture(
-						getResources().getString(R.string.passage_a13),
-						getResources().getString(R.string.passage_b13), 
-						Integer.parseInt(getResources().getString(R.string.passage_c13)), 
-						Integer.parseInt(getResources().getString(R.string.passage_d13))
-				);					
-	    }
+	} 	// End of 	public boolean onOptionsItemSelected(MenuItem item) {
 
-	} // end of private void preInsertScripture(){
-	
-	*/
+	@Override
+	protected Dialog onCreateDialog(int id){
+		Dialog dialog = null;
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		
+		switch(id){
+		case DIALOG_HELP_ID:
+			dialog = createHelpDialog(builder);
+			break;
+		case DIALOG_ABOUT_ID:
+			dialog = createAboutDialog(builder);
+			break;
+		
+		}
+		
+		if(dialog == null)
+			Log.d("Dialog Info:", "Uh oh! Dialog is null");
+		else
+			Log.d("Dialog Info:", "Dialog created: " + id + ", dialog: " + dialog);
+		return dialog;    
+		
+	} // end of 	protected Dialog onCreateDialog(int id){
+
+	private Dialog createAboutDialog(Builder builder) {
+		Context context = getApplicationContext();
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.about_dialog, null); 		
+		builder.setView(layout);
+		builder.setTitle(R.string.about_label);
+		builder.setPositiveButton(R.string.vButton, null);	
+		return builder.create();
+	}
+
+	private Dialog createHelpDialog(Builder builder) {
+		Context context = getApplicationContext();
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.help_dialog, null); 		
+		builder.setView(layout);
+		builder.setTitle(R.string.help_label);
+		builder.setPositiveButton(R.string.vButton, null);	
+		return builder.create();
+	}
 	
 } // end of public class MainActivity extends Activity {
